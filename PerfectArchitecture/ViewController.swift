a3a42020c838a9e0934c79c7af979cbd750efedc
rafael.ruiz@valid.com <rafael.ruiz@valid.com>
2018-12-27 16:10:54 -0500

    Initial Commit

---
+++ PerfectArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerfectArchitecture
+//
+//  Created by valid on 12/27/18.
+//  Copyright © 2018 valid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
